c402def031f532ba92960bcb34308754770f3072
Csaba Fülöp <csfulop@users.noreply.github.com>
2019-05-25 21:47:53 +0200

    binary search

---
+++ algorithms/search/binary_search.py
@@ -0,0 +1,70 @@
+"""
+Given a sorted array arr[] of n elements, write a function to search a given element x in arr[].
+
+https://www.geeksforgeeks.org/binary-search/
+"""
+from random import randint
+from typing import List
+from unittest import TestCase
+
+from hamcrest.core import assert_that
+from hamcrest.core.core import is_
+
+
+def binary_search(list: List, element) -> int:
+    """
+    Using binary search to find element in sorted list and return its position
+    :param list: sorted list
+    :param element: the item to be find
+    :return: position of element in list, raise ValueError if not found
+    """
+    if not list:
+        raise ValueError('%s not found' % element)
+    mid_pos = len(list) // 2
+    mid_val = list[mid_pos]
+    if mid_val == element:
+        return mid_pos
+    elif element < mid_val:
+        return binary_search(list[0:mid_pos], element)
+    else:
+        return binary_search(list[mid_pos + 1:], element) + mid_pos + 1
+
+
+class TestBinarySearch(TestCase):
+    def test_search_empty_list(self):
+        with self.assertRaises(ValueError):
+            binary_search([], 1)
+
+    def test_search(self):
+        assert_that(binary_search([1], 1), is_(0))
+        for i in range(3):
+            assert_that(binary_search([1, 2, 3], i + 1), is_(i))
+        for i in range(4):
+            assert_that(binary_search([1, 2, 3, 4], i + 1), is_(i))
+        for i in range(5):
+            assert_that(binary_search([1, 2, 3, 4, 5], i + 1), is_(i))
+
+    def test_not_found(self):
+        for i in (1, 3):
+            with self.assertRaises(ValueError):
+                binary_search([2], i)
+        for i in (1, 3, 5):
+            with self.assertRaises(ValueError):
+                binary_search([2, 4], i)
+        for i in (1, 3, 5, 7):
+            with self.assertRaises(ValueError):
+                binary_search([2, 4, 6], i)
+
+    def test_big_data(self):
+        N = 100000
+        list = [randint(2, 3)]
+        for i in range(N):
+            list.append(list[-1] + randint(2, 3))
+        for i in range(10):
+            pos = randint(0, len(list) - 1)
+            result = binary_search(list, list[pos])
+            assert_that(result, is_(pos))
+        for i in range(10):
+            pos = randint(0, len(list) - 1)
+            with self.assertRaises(ValueError):
+                binary_search(list, list[pos] + 1)
