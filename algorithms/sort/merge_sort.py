a67e8565ff2b44d5334f5918d2831a69f35e6b15
Csaba Fülöp <csfulop@users.noreply.github.com>
2019-06-20 21:07:49 +0200

    adding merge sort algorithm

---
+++ algorithms/sort/merge_sort.py
@@ -0,0 +1,37 @@
+"""
+Merge Sort is a Divide and Conquer algorithm.
+It divides input array in two halves, calls itself for the two halves and then merges the two sorted halves.
+
+https://www.geeksforgeeks.org/merge-sort/
+"""
+from typing import List
+
+from algorithms.sort import Base
+
+
+def merge_sort(list: List) -> List:
+    if len(list) <= 1:
+        return list
+
+    middle = len(list) // 2
+    first_half = merge_sort(list[:middle])
+    second_half = merge_sort(list[middle:])
+
+    result = []
+    while first_half and second_half:
+        if first_half[0] < second_half[0]:
+            result.append(first_half.pop(0))
+        else:
+            result.append(second_half.pop(0))
+    if first_half:
+        result.extend(first_half)
+    if second_half:
+        result.extend(second_half)
+
+    return result
+
+
+class TestMergeSort(Base.SortBase):
+    def setUp(self) -> None:
+        super().setUp()
+        self.sort = merge_sort
