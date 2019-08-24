34bfeba3f124c9cae6e82fa441453800ee2082ce
Csaba Fülöp <csfulop@users.noreply.github.com>
2019-08-24 20:37:39 +0200

    update and unify comment

--- algorithms/sort/test_heap_sort.py
+++ algorithms/sort/test_heap_sort.py
@@ -1,3 +1,7 @@
+"""
+Heap sort is a comparison based sorting technique based on Binary Heap data structure.
+https://www.geeksforgeeks.org/heap-sort/
+"""
 from typing import List
 
 from algorithms import sort
@@ -5,12 +9,6 @@ from algorithms.data_structure.test_heap import Heap
 
 
 def heap_sort(input: List) -> List:
-    """
-    Heap sort is a comparison based sorting technique based on Binary Heap data structure.
-    https://www.geeksforgeeks.org/heap-sort/
-    :param input: list to be sorted
-    :return: the sorted list
-    """
     heap = Heap(items=input, comparator=int.__lt__)
     result = list()
     while len(heap) > 0:
